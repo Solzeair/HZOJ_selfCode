9e589aad694defb32e55f5de1ebc37d0a1750b6e
Solzeair <3084533622@qq.com>
2024-09-27 00:02:28 +0800

    HZOJ NO.95 refence answer

---
+++ 4.HZOJ_95.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 4.HZOJ_95.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Thu 26 Sep 2024 11:58:59 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%d", n/10 + n%10*10);
+    return 0;
+}
