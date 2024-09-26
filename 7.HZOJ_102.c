8c6d013c9f5bc200ed69a9e2ec96f55dc684b896
Solzeair <3084533622@qq.com>
2024-09-27 00:47:04 +0800

    HZOJ NO.102 reference answer

---
+++ 7.HZOJ_102.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 7.HZOJ_102.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Fri 27 Sep 2024 12:30:58 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    double a, b, c, t;
+    scanf("%lf%lf%lf%lf",&a, &b, &c, &t);
+    printf("%.2lf", t + (1 - (1/a + 1/b) * t) / (1/a + 1/b - 1/c));
+    return 0;
+}
