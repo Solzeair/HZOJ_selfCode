24954907434e58d3fae7e5edaa6f7d89d71a3d62
Solzeair <3084533622@qq.com>
2024-09-27 00:08:16 +0800

    HZOJ NO.97 reference answer

---
+++ 5.HZOJ_97.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 5.HZOJ_97.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Fri 27 Sep 2024 12:03:31 AM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    printf("%lld", n * (n + 1ll) / 2);
+    return 0;
+}
