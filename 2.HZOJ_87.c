0c896a7bbc6c5adf4d84062e52a862d4ca931d00
Solzeair <3084533622@qq.com>
2024-09-26 23:12:09 +0800

    HZOJ NO.87 reference answer

---
+++ 2.HZOJ_87.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	> File Name: 2.HZOJ_87.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Thu 26 Sep 2024 11:05:58 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    float l, w;
+    scanf("%f%f", &l, &w);
+    printf("%.2f\n", (l + w) * 2);
+    printf("%.2f", l * w);
+    return 0;
+}
