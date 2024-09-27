5c1faa9fc5359ea0b62c73d45bb180708d47e77f
Solzeair <3084533622@qq.com>
2024-09-27 20:52:45 +0800

    HZOJ NO.94 reference answer

---
+++ 9.HZOJ_94.c
@@ -0,0 +1,15 @@
+/*************************************************************************
+	> File Name: 9.HZOJ_94.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Fri 27 Sep 2024 08:49:09 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    double w, h;
+    scanf("%lf%lf", &w, &h);
+    printf("%.2lf", w / (h * h));
+    return 0;
+}
