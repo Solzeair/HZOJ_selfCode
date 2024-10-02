89b09bb913d4fd0f46673bb9275304ceb0aef323
Solzeair <3084533622@qq.com>
2024-10-02 22:52:39 +0800

    HZOJ NO.113 reference answer

---
+++ 12.HZOJ_113.c
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: 12.HZOJ_113.c
+	> Author: Sakurarry
+	> Mail: solzeair@163.com
+	> Created Time: Wed 02 Oct 2024 10:19:37 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main() {
+    int y, m;
+    scanf("%d%d", &y, &m);
+    //(y % 4 == 0 && y % 100) || y % 400 == 0 为闰年计算方式
+    if (y % 4 == 0 && y % 100 || y % 400 == 0) {
+        //&&和||为从左到右结合方式，可以不用加括号
+        if (m == 2) printf("29");
+        else if (m == 4 || m == 6 || m == 9 || m == 11) printf("30");
+        else printf("31");
+    }
+    else {
+        if (m == 2) printf("28");
+        else if (m == 4 || m == 6 || m == 9 || m == 11) printf("30"); 
+        else printf("31");
+    }
+    return 0;
+}
