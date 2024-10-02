49e5214bfd7c8c134148f5b1e59a13e8a8dfe1ba
Sakurarry <3084533622@qq.com>
2024-10-02 22:08:16 +0800

    Update 2.HZOJ_87.c

--- 2.HZOJ_87.c
+++ 2.HZOJ_87.c
@@ -11,6 +11,6 @@ int main() {
 	//浮点型尽量用double，减少溢出的可能性，本题测试数据不会溢出
     float l, w;//l，w是length和width的缩写，个人习惯便于辨认
     scanf("%f%f", &l, &w);//scanf函数默认不读入空格，%f之间不加空格不影响
-    printf("%.2f\n%.2f", 2 * (l + w), l * w);
+    printf("%.2f\n%.2f", 2 * (l + w), l * w);//%.2f为保留小数点后2位
     return 0;
 }
