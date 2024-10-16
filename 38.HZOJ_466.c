0d4384b00d89d3f156a5d2d4f0dce27599585703
Solzeair <3084533622@qq.com>
2024-10-16 08:54:12 +0800

    HZOJ NO.466 reference answer

--- 38.HZOJ_466.c
+++ 38.HZOJ_466.c
@@ -28,3 +28,26 @@ int main() {
     printf("%d", func(n));
     return 0;
 }
+
+/*明确代码结构，优化代码
+#include<stdio.h>
+
+int is_palindrome(int x) {
+    int z = x, y = 0;
+    while (x) {
+        y = y * 10 + x % 10;
+        x /= 10;
+    }
+    return y == z;
+}
+
+int main() {
+    int n, ans = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        ans += is_palindrome(i);
+    }
+    printf("%d\n", ans);
+    return 0;
+}
+*/
