1693f46c239274eb0bbeee37361f6a4534a71600
Solzeair <3084533622@qq.com>
2025-01-27 16:39:11 +0800

    update

--- Problem_Euler/Base/1.base_problem_001.c
+++ Problem_Euler/Base/1.base_problem_001.c
@@ -4,9 +4,10 @@
 	> Mail: solzeair@gmail.com
 	> Created Time: Mon 27 Jan 2025 03:37:28 PM CST
  ************************************************************************/
-
+/*
 #include<stdio.h>
 
+// 注意要加括号，不然会出错，变成 !n % 3 == 0 || n % 5 == 0
 #define isbeishu(n) (n % 3 == 0 || n % 5 == 0)
 
 void func(int n) {
@@ -22,4 +23,15 @@ int main() {
     func(1000);
     return 0;
 }
+*/
 
+// 更优解
+#include <stdio.h>
+
+int main() {
+    int sum_3 = (3 + 999) * (999 / 3) / 2;
+    int sum_5 = (5 + 995) * (999 / 5) / 2;
+    int sum_15 = (15 + 999 / 15 * 15) * (999 / 15) / 2;
+    printf("%d\n", sum_3 + sum_5 - sum_15);
+    return 0;
+}
