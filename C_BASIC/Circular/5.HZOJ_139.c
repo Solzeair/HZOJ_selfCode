40f8f1fd85197bce0d1ad8bb001b2cb72ca99c4d
Solzeair <3084533622@qq.com>
2025-02-08 10:40:32 +0800

    update

--- C_BASIC/Circular/5.HZOJ_139.c
+++ C_BASIC/Circular/5.HZOJ_139.c
@@ -12,13 +12,13 @@ int main() {
     scanf("%d", &n);
     for (int i = 0; i < n; i++) {//第0行，0到n-1行
         for (int j = 0; j < (n - 1) - i; j++) printf(" ");//第i行和第n-1行隔多少行，每隔一行打印一次
-        for (int j = 0; j <= i - 0; j++) printf("AA");//第i行和第0行隔多少行，每隔一行打印i次，且多打印一次
+        for (int j = 0; j < i + 1; j++) printf("AA");//第 i 行打印 i+1 一次
         printf("\n");
     }
     for (int i = n; i < 2 * n - 1; i++) {
         for (int j = 0; j < i - (n - 1); j++) printf(" ");
-        for (int j = 0; j <= (2 * n - 2) - i ; j++) printf("AA");
-        if (i < 2 * n - 2) printf("\n");//末尾不换行
+        for (int j = 0; j < ((2 * n - 1 - 1) - i) + 1; j++) printf("AA");
+        if (i < 2 * n - 1 - 1) printf("\n");//末尾不换行
     }
     return 0;
 }
